8db62ce7d205e0dd8bca77826203809ff15a477a
Zachary Wenner <zach@flatsmith.com>
2020-04-04 17:18:02 -0700

    Finalize with XML formatting

--- handler.js
+++ handler.js
@@ -1,23 +1,38 @@
 'use strict';
 const Airtable = require('airtable');
 
+let xml_special_to_escaped_one_map = {
+  '&': '&amp;',
+  '"': '&quot;',
+  '<': '&lt;',
+  '>': '&gt;'
+};
+function encodeXml(string) {
+  if(!string){
+    return '';
+  }
+  return string.toString().replace(/([\&"<>])/g, function(str, item) {
+      return xml_special_to_escaped_one_map[item];
+  });
+};
+
 async function fetchAirtableRecords() {
   try {
     // Get Airtable Base
     const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_ID);
     
     // Get Podcast Record
-    let podcastRecord = await base('Podcast').select({maxRecords: 1}).all()[0];
+    let podcastRecords = await base('Podcast').select({maxRecords: 1}).all();
 
     // Get All Episode Records
     let episodeRecords = [];
     await base('Episodes').select({
       sort: [
-        {field: "Season", direction: "desc"},
-        {field: "Episode", direction: "desc"}
+        {field: "<itunes:season>", direction: "desc"},
+        {field: "<itunes:episode>", direction: "desc"}
       ],
       maxRecords: 100,
-      filterByFormula: `DATETIME_DIFF(NOW(),pubDate,'minutes')>0`
+      filterByFormula: `DATETIME_DIFF(NOW(),{<pubDate>},'minutes')>0`
     }).eachPage(function page(records, fetchNextPage) {
         episodeRecords = episodeRecords.concat(records);
         // Get more than 100 records
@@ -33,7 +48,7 @@ async function fetchAirtableRecords() {
     const subcategoryRecords = await base('iTunes Subcategories').select({ maxRecords: 100 }).all();
 
     return {
-      podcast: podcastRecord,
+      podcast: podcastRecords[0],
       episodes: episodeRecords,
       categories: categoryRecords,
       subcategories: subcategoryRecords,
@@ -41,6 +56,34 @@ async function fetchAirtableRecords() {
   } catch (err) {
     console.log(err);
   }
+}
+
+function episodeXML(episode) {
+  return `
+  <item>
+    <title>${encodeXml(episode['<title>'])}</title>
+    <itunes:title>${encodeXml(episode['<title>'])}</itunes:title>
+    ${episode['<link>'] ? `<link>${encodeXml(episode['<link>'])}</link>` : ''}
+    ${episode['<comments>'] ? `<comments>${encodeXml(episode['<comments>'])}</comments>` : ''}
+    <pubDate>${encodeXml(new Date(episode['<pubDate>']).toUTCString())}</pubDate>
+    <guid isPermaLink="false">${encodeXml(episode['<title>'])}</guid>
+    <description><![CDATA[${encodeXml(episode['<description>'])}]]></description>
+    <content:encoded><![CDATA[${encodeXml(episode['<description>'])}]]></content:encoded>           
+    <enclosure url="${encodeXml(episode['<enclosure url>'])}" length="${encodeXml(episode['<enclosure length>'])}" type="${encodeXml(episode['<enclosure type>'])}"/>
+    <itunes:summary>${encodeXml(episode['<description>'])}</itunes:summary>
+    <itunes:author>${encodeXml(episode['<itunes:author>'])}</itunes:author>
+    <itunes:explicit>${encodeXml(episode['<itunes:explicit>'])}</itunes:explicit>
+    <itunes:block>${encodeXml(episode['<itunes:block>'])}</itunes:block>
+    <itunes:duration>${encodeXml(episode['<itunes:duration>'])}</itunes:duration>
+    <itunes:season>${encodeXml(episode['<itunes:season>'])}</itunes:season>
+    <itunes:episode>${encodeXml(episode['<itunes:episode>'])}</itunes:episode>
+    <itunes:episodeType>${encodeXml(episode['<itunes:episodeType>'])}</itunes:episodeType>
+    <itunes:image href="${encodeXml(episode['<itunes:image>'])}" />
+  </item>`;
+}
+
+function categoryXML(category,subcategory) {
+  return `<itunes:category text="${encodeXml(category)}"><itunes:category text="${encodeXml(subcategory)}"/></itunes:category>`;
 }
 
 module.exports.feed = async event => {
@@ -51,6 +94,18 @@ module.exports.feed = async event => {
     categories,
     subcategories
   } = await fetchAirtableRecords();
+
+  // Format categories into XML
+  const categoryItems = categories.map((category) => {
+    // Find subcategory from ID
+    const subcategory = subcategories.find((record) => record.id === category.fields['<itunes:subcategory>'][0]);
+    return categoryXML(category.fields['<itunes:category>'], subcategory.fields['<itunes:subcategory>']);
+  });
+
+  // Format episodes into XML
+  const episodeItems = episodes.map((episode) => {
+    return episodeXML(episode.fields);
+  });
 
   const body = `<?xml version="1.0" encoding="UTF-8"?>
   <rss version="2.0"
@@ -67,12 +122,12 @@ module.exports.feed = async event => {
       <atom:link href="${podcast.fields['<atom:link>']}" rel="self" type="application/rss+xml"/>
       <link>${podcast.fields['<link>']}</link>
       <description>${podcast.fields['<description>']}</description>
-      <language>${podcast.fields['<description>']}</language>
+      <language>${podcast.fields['<language>']}</language>
       <spotify:countryOfOrigin>us</spotify:countryOfOrigin>
       <copyright>${podcast.fields['<copyright>']}</copyright>
       <ttl>1440</ttl>
       <itunes:type>${podcast.fields['<itunes:type>']}</itunes:type>
-      <itunes:author>${podcast.fields['itunes:author']}</itunes:author>
+      <itunes:author>${podcast.fields['<itunes:author>']}</itunes:author>
       <itunes:owner>
           <itunes:name>${podcast.fields['<itunes:author>']}</itunes:name>
           <itunes:email>${podcast.fields['<itunes:email>']}</itunes:email>
@@ -80,6 +135,8 @@ module.exports.feed = async event => {
       <itunes:block>${podcast.fields['<itunes:block>']}</itunes:block>
       <itunes:explicit>${podcast.fields['<itunes:explicit>']}</itunes:explicit>
       <itunes:image href="${podcast.fields['<itunes:image>']}" />
+      ${categoryItems.join('\n')}
+      ${episodeItems.join('\n')}
   </channel>
   </rss>
   `;
